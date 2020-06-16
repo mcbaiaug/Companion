28e45b603d49598a74c9e804e35f5acccbe9a58e
August <mcbaiaug@gmail.com>
2020-06-16 17:49:44 -0500

    Route Work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Nav from './components/Nav'
+import About from './pages/About'
+import Shop from './pages/Shop'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <Nav />
+        <switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/shop" component={Shop} />
+        </switch>
+      </div>
+    </Router>
+
+  )
 }
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+)
+
+
 
 export default App;
---
+++ src/pages/Shop.js
@@ -0,0 +1,25 @@
+import React,{useState, useEffect} from 'react'
+
+
+function Shop() {
+
+
+    useEffect(() => {
+        fetchItems()
+    },[])
+
+    const fetchItems = async () => {
+        const data = await fetch ('https://fortnite-public-api.theapinetwork.com/prod09/upcoming/get')
+        const items = await data.json()
+        console.log(items)
+    }
+
+  return (
+    <div>
+        <h1>Shop Page</h1>
+    </div>
+
+  )
+}
+
+export default Shop;
