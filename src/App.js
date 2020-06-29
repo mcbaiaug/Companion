20fa56a6f3f2613673a1e4f72a773a6697d8519e
August <mcbaiaug@gmail.com>
2020-06-29 11:48:01 -0500

    Styling Change

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './pages/About'
 import Shop from './pages/Shop'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
+import Admin from './pages/Admin'
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/about" component={About} />
           <Route path="/shop" component={Shop} />
+          <Route path="/Admin" component={Admin} />
         </Switch>
       </div>
     </Router>
